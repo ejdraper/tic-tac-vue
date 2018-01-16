60a3f142c2006eaf5e14318f5cd2bad5cfb6c0aa
Elliott Draper <el@kickcode.com>
2018-01-16 13:53:21 +0000

    adding win logic and a message to show who won

--- app.js
+++ app.js
@@ -7,17 +7,37 @@ var Grid = Vue.component('grid', {
         ['', '', ''],
         ['', '', '']
       ],
-      next: 'x'
+      next: 'x',
+      finished: false
     };
   },
   methods: {
     tap: function(e) {
-      if (e.target.innerText == '') {
+      if (!this.finished && e.target.innerText == '') {
         let rows = this.rows;
         rows[e.target.attributes['data-row'].value][e.target.attributes['data-column'].value] = this.next;
         this.rows = rows.slice(0);
-        this.next = (this.next == 'x' ? 'o' : 'x');
+
+        if (this.checkWinner()) {
+          this.finished = true
+        } else {
+          this.next = (this.next == 'x' ? 'o' : 'x');
+        }
       }
+    },
+    checkWinner: function() {
+      return (
+        this.checkValues(this.rows[0]) ||
+        this.checkValues(this.rows[1]) ||
+        this.checkValues(this.rows[2]) ||
+        this.checkValues([this.rows[0][0], this.rows[1][0], this.rows[2][0]]) ||
+        this.checkValues([this.rows[0][1], this.rows[1][1], this.rows[2][1]]) ||
+        this.checkValues([this.rows[0][2], this.rows[1][2], this.rows[2][2]]) ||
+        this.checkValues([this.rows[0][0], this.rows[1][1], this.rows[2][2]]) ||
+        this.checkValues([this.rows[0][2], this.rows[1][1], this.rows[2][0]]));
+    },
+    checkValues: function(values) {
+      return (values[0] != '' && values[1] != '' && values[2] != '' && (values[0] === values[1]) && (values[1] === values[2]));
     }
   }
 });
